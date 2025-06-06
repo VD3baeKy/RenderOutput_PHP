e78141c79e0537a046fb22bd9c324a2003ea48ef
VD3baeKy <digfehc@gmail.com>
2025-06-06 21:50:07 +0900

    Update read.php

--- src/read.php
+++ src/read.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo getenv('PGHOST');
+echo "${PGHOST}";
 var_dump($_ENV['PGHOST'], getenv('PGHOST'));
 
     
