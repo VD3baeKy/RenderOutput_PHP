39ecde76f2d5ea65a849fac5dc2cd0931abb7254
VD3baeKy <digfehc@gmail.com>
2025-06-07 00:53:42 +0900

    Create create_functions.php

---
+++ test-code/create_functions.php
@@ -0,0 +1,27 @@
+<?php
+function getPdo($dsn, $db_user, $db_password) {
+    $pdo = new PDO($dsn, $db_user, $db_password);
+    $pdo->exec("SET NAMES 'UTF8'");
+    return $pdo;
+}
+
+function createProduct($pdo, $data) {
+    $sql = '
+        INSERT INTO products (product_code, product_name, price, stock_quantity, vendor_code)
+        VALUES (:product_code, :product_name, :price, :stock_quantity, :vendor_code)
+    ';
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':product_code', $data['product_code'], PDO::PARAM_INT);
+    $stmt->bindValue(':product_name', $data['product_name'], PDO::PARAM_STR);
+    $stmt->bindValue(':price', $data['price'], PDO::PARAM_INT);
+    $stmt->bindValue(':stock_quantity', $data['stock_quantity'], PDO::PARAM_INT);
+    $stmt->bindValue(':vendor_code', $data['vendor_code'], PDO::PARAM_INT);
+    $stmt->execute();
+    return $stmt->rowCount();
+}
+
+function getAllVendorCodes($pdo) {
+    $sql = 'SELECT vendor_code FROM vendors';
+    $stmt = $pdo->query($sql);
+    return $stmt->fetchAll(PDO::FETCH_COLUMN);
+}
