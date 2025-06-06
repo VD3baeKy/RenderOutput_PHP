62a2dcffbe19f2234745e3323be47d8de0a31c00
VD3baeKy <digfehc@gmail.com>
2025-06-06 20:20:28 +0900

    Update update.php

--- src/update.php
+++ src/update.php
@@ -13,12 +13,12 @@ $db_port = $_ENV['DB_PORT'] ?? '5432';
 
 // PostgreSQL用のDSN
 $dsn = "pgsql:host={$db_host};port={$db_port};dbname={$db_name}";
-$pdo->exec("SET NAMES 'UTF8'");
 
 // submitパラメータの値が存在するとき（「更新」ボタンを押したとき）の処理
 if (isset($_POST['submit'])) {
     try {
-        $pdo = new PDO($dsn, $user, $password);
+        $pdo = new PDO($dsn, $db_user, $db_password);
+        $pdo->exec("SET NAMES 'UTF8'");
 
         // 動的に変わる値をプレースホルダに置き換えたUPDATE文をあらかじめ用意する
         $sql_update = '
