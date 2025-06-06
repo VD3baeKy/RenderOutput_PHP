4d461682bd8d0b0615d6a3b6bd8d0cb65a7e5f6d
VD3baeKy <digfehc@gmail.com>
2025-06-06 23:26:44 +0900

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 
+# Secretファイルの権限を修正
+if [ -f /etc/secrets/.env ]; then
+    echo "[start.sh] Fixing permissions for /etc/secrets/.env"
+    chown www-data:www-data /etc/secrets/.env
+    chmod 640 /etc/secrets/.env
+fi
+
 # Nginxのconfを環境変数PORTで置換
 envsubst '$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
