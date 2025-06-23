ad43129732aa762d4f3f15dd6aef2a68b282bbca
VD3baeKy <digfehc@gmail.com>
2025-06-23 12:47:30 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# /tmp ディレクトリに全ユーザーがアクセスできるようにパーミッションを設定
+# これにより、www-dataユーザーが/tmpディレクトリ内に入ってファイルを探せるようになる
+chmod 1777 /tmp
+echo "[start.sh] Set permissions for /tmp directory"
+
 # Secretファイルをweb公開外領域（/tmp）にコピー
 if [ -f /etc/secrets/.env ]; then
     cp /etc/secrets/.env /tmp/.env
